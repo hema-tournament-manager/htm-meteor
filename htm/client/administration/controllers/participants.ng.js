b4a3d9bf7b4500c8eee8533f1eb9599237a7171c
mpkorstanje <rien.korstanje@gmail.com>
2015-08-19 18:05:49 +0200

    Using closures and array syntax or minification breaks.

--- htm/client/administration/controllers/participants.ng.js
+++ htm/client/administration/controllers/participants.ng.js
@@ -1,72 +1,81 @@
-angular.module('htm.administration').controller('ParticipantsCtrl', ParticipantsController);
-angular.module('htm.administration').controller('ParticipantsImportCtrl', ParticipantsImportController);
 
-function ParticipantsController($meteor) {
+angular.module('htm.administration').controller('ParticipantsCtrl', ['$meteor',function($meteor) {
+  
   this.list = $meteor.collection(Participants);
-}
 
-ParticipantsController.prototype.add = function() {
-  this.list.push({number: this.list.length + 1, name: 'Jogchem Dijkstra', country: {code: 'NL', name: 'Netherlands'}, club: {name: 'HTM'}});
-};
-
-function ParticipantsImportController($scope, $element, $window, $meteor, $state) {
-  this.hasFile = false;
-  this.$meteor = $meteor;
-  this.$state = $state;
-  var self = this;
-  var drop = $element.find('#drop');
-  var handleDragOver = function(jqe) {
-    var e = jqe.originalEvent || jqe;
-    e.stopPropagation();
-    e.preventDefault();
-    e.dataTransfer.dropEffect = 'copy';
+  this.add = function() {
+    this.list.push({number: this.list.length + 1, name: 'Jogchem Dijkstra', country: {code: 'NL', name: 'Netherlands'}, club: {name: 'HTM'}});
   };
-  drop.bind('dragenter', handleDragOver);
-  drop.bind('dragover', handleDragOver);
-  drop.bind('drop', function(jqe) {
-    var e = jqe.originalEvent || jqe;
-    e.stopPropagation();
-    e.preventDefault();
-    var files = e.dataTransfer.files;
-    var f = files[0];
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      var data = e.target.result;
-      var wb = XLSX.read(data, {type: 'binary'});
-      $scope.$apply(function() {
-        self.hasFile = true;
-        self.participants = wb.SheetNames.reduce(function(aggregator, sheet) {
-          var rows = XLSX.utils.sheet_to_row_object_array(wb.Sheets[sheet]);
-          return aggregator.concat(rows.filter(function(p) {
-            return p.Name;
-          }).map(function(p) {
-            return {
-              name: p.Name,
-              club: p.Club,
-              country: p.Country,
-              selected: true
-            };
-          }));
-        }, []);
-      });
-    };
-    reader.readAsBinaryString(f);
-  });
-}
 
-ParticipantsImportController.prototype.import = function() {
-  var participants = this.participants;
-  var coll = this.$meteor.collection(Participants);
-  console.log(coll);
-  participants.map(function(p) {
-    return {
-      name: p.name,
-      club: {name: p.club},
-      country: {code: p.country}
+
+}]);
+
+angular.module('htm.administration').controller('ParticipantsImportCtrl', 
+  ['$scope', '$element','$window', '$meteor', '$state', 
+  function ParticipantsImportController($scope, $element, $window, $meteor, $state) {
+    
+    this.hasFile = false;
+   
+    this.import = function() {
+      var participants = this.participants;
+      var coll = $meteor.collection(Participants);
+      console.log(coll);
+      participants.map(function(p) {
+        return {
+          name: p.name,
+          club: {name: p.club},
+          country: {code: p.country}
+        };
+      }).forEach(function(p) {
+        coll.save(p);
+      });
+      coll.stop();
+      $state.go('administration.participants');
     };
-  }).forEach(function(p) {
-    coll.save(p);
-  });
-  coll.stop();
-  this.$state.go('administration.participants');
-};
+
+    var handleDragOver = function(jqe) {
+      var e = jqe.originalEvent || jqe;
+      e.stopPropagation();
+      e.preventDefault();
+      e.dataTransfer.dropEffect = 'copy';
+    };
+
+    var self = this;
+    var readParticpants = function(jqe) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        var data = e.target.result;
+        var wb = XLSX.read(data, {type: 'binary'});
+        $scope.$apply(function() {
+          self.hasFile = true;
+          self.participants = wb.SheetNames.reduce(function(aggregator, sheet) {
+            var rows = XLSX.utils.sheet_to_row_object_array(wb.Sheets[sheet]);
+            return aggregator.concat(rows.filter(function(p) {
+              return p.Name;
+            }).map(function(p) {
+              return {
+                name: p.Name,
+                club: p.Club,
+                country: p.Country,
+                selected: true
+              };
+            }));
+          },[]);
+        });
+      };
+
+      var e = jqe.originalEvent || jqe;
+      e.stopPropagation();
+      e.preventDefault();
+      var files = e.dataTransfer.files;
+      var f = files[0];
+      
+      reader.readAsBinaryString(f);
+    }
+    
+    var drop = $element.find('#drop');
+    drop.bind('dragenter', handleDragOver);
+    drop.bind('dragover', handleDragOver);
+    drop.bind('drop', readParticpants);
+}]);
+
