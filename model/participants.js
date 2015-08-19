fff407bbf76ab1b5b832ccdb297cbc8bc73228cd
Jogchem Dijkstra <jogchem@gmail.com>
2015-08-19 21:06:06 +0200

    Added method to insert participants, so they get their number automatically

--- model/participants.js
+++ model/participants.js
@@ -1,1 +1,13 @@
 Participants = new Mongo.Collection('participants');
+
+Meteor.methods({
+  insertParticipants: function(participants) {
+    var q = Participants.find();
+    participants.forEach(function(p) {
+      if (!p.number) {
+        p.number = q.count() + 1;
+      }
+      Participants.insert(p);
+    });
+  }
+});
