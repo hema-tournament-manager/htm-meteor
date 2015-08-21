0262cf3d911cd66f5f6e39ca4132a3bad1fb46ca
mpkorstanje <rien.korstanje@gmail.com>
2015-08-21 21:40:49 +0200

    Highlighting search results

--- client/core/lib/module.ng.js
+++ client/core/lib/module.ng.js
@@ -1,1 +1,13 @@
 angular.module('htm.core', []);
+
+
+angular.module('htm.core')
+	  .filter('highlight', function($sce) {
+    return function(text, phrase) {
+      if (phrase && text) {
+      	text = text.replace(new RegExp('('+phrase+')', 'gi'),'<span class="highlighted">$1</span>');
+      }
+
+      return $sce.trustAsHtml(text)
+    }
+  });
