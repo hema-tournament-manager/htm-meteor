6e28f4ad0492db63f7d06406944f9ac2cba990c8
mpkorstanje <rien.korstanje@gmail.com>
2015-10-02 20:41:20 +0200

    Ported test from Chai to Jasmine (default for cucumber)

--- tests/cucumber/features/step_definitions/steps.js
+++ tests/cucumber/features/step_definitions/steps.js
@@ -15,99 +15,82 @@
     var url = require('url');
 
     this.Given(/^I am a new user$/, function () {
-      return this.server.call('reset');
+      server.call('reset');
     });
 
     // Given the Land of Ooo exists will call createTheLandOfOoo
     // Given Finn exists will call createFinn
     this.Given(/^(.*) exists$/, function (entity) {
-      return this.server.call(camelize('create ' + entity));
+      server.call(camelize('create ' + entity));
     });
 
-
     this.When(/^I navigate to "([^"]*)"$/, function (relativePath) {
-      return this.client.
-        url(url.resolve(process.env.ROOT_URL, relativePath));
+      client.url(url.resolve(process.env.ROOT_URL, relativePath));
     });
 
     // For buttons w/ a title
     this.When(/^I click (button|link) "([^"]*)"$/, function (buttonOrLink, linkText) {
-      return this.client.click((buttonOrLink === 'button' ? buttonOrLink : 'a') + '=' + linkText);
+      client.click((buttonOrLink === 'button' ? buttonOrLink : 'a') + '=' + linkText);
     });
 
     // For buttons with a name w/o a title or icon
     this.When(/^I click (.*) button$/, function (name) {
-      return this.client.click("button[name=\"" + name + "\"]");
+      client.click("button[name=\"" + name + "\"]");
     });
+    
     // For buttons with an icon w/o a title or name
     this.When(/^I click the ([^ ]*) icon$/, function (icon) {
-      return this.client.click(".glyphicon-" + icon);
+      client.click(".glyphicon-" + icon);
     });
-
 
     this.Then(/^I should see the title "([^"]*)"$/, function (expectedText) {
-      return this.client
-        .waitForVisible('h1,h2,h3,h4,h5,h6')
-        .getText('*').should.eventually.match(new RegExp(expectedText, 'g'));
-     
+      client.waitForVisible('h1,h2,h3,h4,h5,h6');
+      expect(client.getText('h1,h2,h3,h4,h5,h6')).toMatch(new RegExp(expectedText, 'g'));     
     });
+
     this.Then(/^I should not see the title "([^"]*)"$/, function (expectedText) {
-      return this.client
-        .waitForVisible('h1,h2,h3,h4,h5,h6')
-        .getText('*').should.eventually.not.match(new RegExp(expectedText, 'g'));
+      client.waitForVisible('h1,h2,h3,h4,h5,h6');
+      expect(client.getText('h1,h2,h3,h4,h5,h6')).not.toMatch(new RegExp(expectedText, 'g'));
     });
 
     this.Then(/^I should see the entry "([^"]*)"$/, function (expectedText) {
-      return this.client
-        .waitForVisible('td')
-        .getText('*').should.eventually.match(new RegExp(expectedText, 'g'));
+      client.waitForVisible('td');
+      expect(client.getText('td')).toMatch(new RegExp(expectedText, 'g'));
     });
+
     this.Then(/^I should not see the entry "([^"]*)"$/, function (expectedText) {
-      return this.client
-        .waitForVisible('td')
-        .getText('*').should.eventually.not.match(new RegExp(expectedText, 'g'));
+      client.waitForVisible('td');
+      expect(client.getText('td')).not.toMatch(new RegExp(expectedText, 'g'));
     });
 
     this.Then(/^I should see the label "([^"]*)"$/, function (expectedText) {
-      return this.client
-        .waitForVisible('label')
-        .getText('*').should.eventually.match(new RegExp(expectedText, 'g'));
+      client.waitForVisible('label');
+      expect(client.getText('label')).toMatch(new RegExp(expectedText, 'g'));
     });
     this.Then(/^I should not see the label "([^"]*)"$/, function (expectedText) {
-      return this.client
-        .waitForVisible('label')
-        .getText('*').should.eventually.not.match(new RegExp(expectedText, 'g'));
+      client.waitForVisible('label');
+      expect(client.getText('label')).not.toMatch(new RegExp(expectedText, 'g'));
     });
 
     this.Then(/^I should see the text "([^"]*)"$/, function (expectedText) {
-      return this.client
-        .waitForVisible('body')
-        .getText('*').should.eventually.match(new RegExp(expectedText, 'g'));
+      client.waitForVisible('body');
+      expect(client.getText('body')).toMatch(new RegExp(expectedText, 'g'));
      
     });
     this.Then(/^I should not see the text "([^"]*)"$/, function (expectedText) {
-     return this.client
-       .waitForVisible('body')
-       .getText('*').should.eventually.not.match(new RegExp(expectedText, 'g'));
+      client.waitForVisible('body');
+      expect(client.getText('body')).not.toMatch(new RegExp(expectedText, 'g'));
     });
 
     this.When(/^I enter the (.*): "([^"]*)"$/, function (field, value) {
       var element = "input[name=\""+field+"\"] ";
-      return this.client.clearElement(element).setValue(element,value);
+      client.clearElement(element);
+      client.setValue(element,value);
     });
 
     this.When(/^I select the (.*): "([^"]*)"$/, function (option, value) {
-      return this.client
-        .click("div[name=\""+option+"\"] .ui-select-match span")
-        .click("span="+value);
-    });
-
-    this.When(/^I drag "[^"]*" and drop it on "[^"]*"$/, function (relativePath) {
-      
-
-
-      return this.client.
-        url(url.resolve(process.env.ROOT_URL, relativePath));
+      client.click("div[name=\""+option+"\"] .ui-select-match span");
+      client.click("span="+value);
     });
 
   };
